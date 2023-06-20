7a3e4eb6067e74eaedc23f02f8a94ae0831e68af
brandonedwinogola <brandon14ogola@gmail.com>
2023-06-20 10:16:32 +0300

    complete

--- basic-template/basic-template/src/App.js
+++ basic-template/basic-template/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Services from './pages/Services';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import {FaBeer} from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" exact element={<Services />} />
+          <Route path="/contact" exact element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
